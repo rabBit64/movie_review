152e09c5645360069bd9844389ce5aa489d4db90
lemon-lime-honey <bs0308@g.skku.edu>
2023-04-13 17:46:08 +0900

    Add profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('delete/', views.delete, name='delete'),
     path('update/', views.update, name='update'),
     path('password/', views.password, name='password'),
+    path('profile/<int:pk>/', views.profile, name='profile'),
 ]
